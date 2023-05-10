6dfeaaf2822f2f0f4b3d954f0f90ddb571ce0378
yih1612 <yih1612@naver.com>
2023-05-10 17:20:03 +0900

    add, checked

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import TodoListItem from "./components/TodoListItem";
 
-function App() {
+export default function App() {
+  const [list, setList] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setTodo(value);
+  };
+  const handleAdded = () => {
+    setList((prev) => [...prev, { name: todo }]);
+    setTodo("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoListItem list={list} />
+      <input onChange={handleChange} value={todo}></input>
+      <button onClick={handleAdded}>추가</button>
+    </>
   );
 }
-
-export default App;
