96ab243b044cd694a2bb5ac50816848de41a5307
yih1612 <yih1612@naver.com>
2023-05-12 17:26:23 +0900

    8.11 filter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header/Header";
 import TodoList from "./components/TodoList/TodoList";
 
+const filters = ["all", "active", "completed"];
+
 export default function App() {
+  const [filter, setFilter] = useState(filters[0]);
   return (
     <>
-      <TodoList />
+      <Header filters={filters} filter={filter} onFilterChange={setFilter} />
+      <TodoList filter={filter} />
     </>
   );
 }
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
 import Todo from "../Todo/Todo";
 
-export default function TodoList() {
+export default function TodoList({ filter }) {
   const [todos, setTodos] = useState([
     { id: "123", text: "장보기", status: "active" },
     { id: "124", text: "공부하기", status: "active" },
@@ -13,11 +13,13 @@ export default function TodoList() {
   const handleDelete = (deleted) => {
     setTodos(todos.filter((t) => t.id !== deleted.id));
   };
+
+  const filtered = getFilteredItems(todos, filter);
   return (
     <div>
       <section>
         <ul>
-          {todos.map((item) => (
+          {filtered.map((item) => (
             <Todo
               key={item.id}
               todo={item}
@@ -30,4 +32,11 @@ export default function TodoList() {
       </section>
     </div>
   );
+}
+
+function getFilteredItems(todos, filter) {
+  if (filter === "all") {
+    return todos;
+  }
+  return todos.filter((todo) => todo.status === filter);
 }
